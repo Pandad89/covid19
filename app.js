ca8871a6dc3d5cbd7a676ff1e99fc2e5a0275fbc
Pandad89 <adambiren19@gmail.com>
2021-12-12 01:50:20 +0200

    lateNightUpdate

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-import { myChart as graph} from './graphs.js';
+// fetch('https://restcountries.herokuapp.com/api/v1')
+// .then((result) => {
+//     return result.json()
+// })
 
-function drawGraph(){
-    
-}
+// a fetch api code
+fetch('https://corona-api.com/countries')
+.then((result) => {
+     return result.json();
+})
+.then((data) => {
+    data.data.forEach(information => {
+        console.log(`${information.name}: Confirmed cases: ${information.latest_data.confirmed}, Critical cases: ${information.latest_data.critical}, Deaths: ${information.latest_data.deaths}, Recoveries: ${information.latest_data.recovered}`)
+    });  
+})
+.catch((err) => {
+    console.log(err)
+})
